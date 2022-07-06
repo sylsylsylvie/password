343d66dc4ea9ca18d0ae9157b5412685af1b1050
Sylvie Liaw <sylvieliaw@gmail.com>
2022-07-05 20:27:17 -0700

    final version

--- password.py
+++ password.py
@@ -1,11 +1,14 @@
 password = '123'
 x = 3
 while x > 0:
+    x = x - 1
     attempt = input('please enter password:')
     if attempt == password:
         print('login successful!')
         break
     else:
-        x = x - 1
-        print('密碼錯誤！還有', x, '次機會')
-
+        print('密碼錯誤！')
+        if x > 0:
+            print('還有', x, '次機會')
+        else:
+            print('錯誤太多次')
